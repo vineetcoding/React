69524da4d1e0675ac03275406365f5e5e7661a13
vineetcoding <114997519+vineetcoding@users.noreply.github.com>
2023-01-06 00:19:39 +0530

    reactDemoFooter

---
+++ src/footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css"
+
+export default function footer(){
+    return(
+        <div className="footer_div">
+            hello from footer
+        </div>
+    )
+}
